ef387bf31ddfe20808821089f88bf7e056f438c8
PerplexDD <53498392+PerplexDD@users.noreply.github.com>
2020-05-01 10:59:56 -0400

    Add files via upload

---
+++ DistanceCalculator/Form1.Designer.vb
@@ -0,0 +1,91 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtDistDisplay = New System.Windows.Forms.TextBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(12, 300)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(111, 38)
+        Me.btnCalc.TabIndex = 0
+        Me.btnCalc.Text = "Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(256, 300)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(111, 38)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'txtDistDisplay
+        '
+        Me.txtDistDisplay.BackColor = System.Drawing.SystemColors.Window
+        Me.txtDistDisplay.Location = New System.Drawing.Point(38, 25)
+        Me.txtDistDisplay.MaximumSize = New System.Drawing.Size(297, 250)
+        Me.txtDistDisplay.MinimumSize = New System.Drawing.Size(297, 250)
+        Me.txtDistDisplay.Multiline = True
+        Me.txtDistDisplay.Name = "txtDistDisplay"
+        Me.txtDistDisplay.ReadOnly = True
+        Me.txtDistDisplay.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtDistDisplay.Size = New System.Drawing.Size(297, 250)
+        Me.txtDistDisplay.TabIndex = 2
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(139, 300)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(111, 38)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(379, 362)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.txtDistDisplay)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Name = "Form1"
+        Me.Text = "Distance Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents txtDistDisplay As TextBox
+    Friend WithEvents btnClear As Button
+End Class
---
+++ DistanceCalculator/Form1.vb
@@ -0,0 +1,77 @@
+﻿'Distance Calculator
+'William Butler 
+'1/10/20
+'A short program to display accumulative distance travled and total distance traveled
+
+Option Strict On
+
+Public Class Form1
+
+    '   Basic exit program button
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    '   Clears the textbox
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtDistDisplay.Text = String.Empty
+    End Sub
+
+    '   Calculates the traveled distance
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+
+        Try
+
+            '   Declare needed variables and inputboxes
+            Dim speed As Integer
+            Dim hours As Integer
+            speed = CInt(InputBox("Enter the Speed:", "Speed"))
+            hours = CInt(InputBox("Enter the Hours traveled:", "Hours"))
+            Dim milesTraveled As Integer = speed * hours
+
+            '   Quick conditional to make sure no negative integers
+            If speed < 0 Or hours < 0 Then
+                MessageBox.Show("Positive numbers only.",
+                                "Error")
+                txtDistDisplay.Text = String.Empty
+
+            Else
+
+                '   Declare the string for the textbox
+                Dim distoutput As String = "Vehicle Speed: " & speed & " MPH" & Environment.NewLine
+                distoutput &= "Time Traveled: " & hours & " Hours" & Environment.NewLine
+                distoutput &= "Hours     Distance Traveled" & Environment.NewLine
+                distoutput &= "-------------------------------------" & Environment.NewLine
+
+                '   Basic for loop in the range of 1 to amount of hours traveled with string added to textbox each iteration
+                '   to show hours relative to distance traveled
+                For i As Integer = 1 To hours
+
+                    distoutput &= CStr(i) & "             " & CStr(speed * i) & Environment.NewLine
+
+                Next
+
+                '   Outputs the string needed for the textbox after all is complete
+                distoutput &= "Total Distance: " & CStr(milesTraveled) & " Miles"
+                txtDistDisplay.Text = distoutput
+
+            End If
+
+
+
+        Catch ex As System.InvalidCastException
+
+            MessageBox.Show("Please use numbers only.",
+                            "Error")
+
+        Catch ex As Exception
+
+            MessageBox.Show("An unknown error as occured, try again.",
+                            "Error")
+
+        End Try
+
+
+    End Sub
+
+End Class
