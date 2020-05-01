c4b2733715b86176882c4c549ce661a13004d66c
PerplexDD <53498392+PerplexDD@users.noreply.github.com>
2020-05-01 11:01:52 -0400

    Add files via upload

---
+++ EmployeeData/Form1.Designer.vb
@@ -0,0 +1,285 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.cboDeptName = New System.Windows.Forms.ComboBox()
+        Me.txtEmailAddy = New System.Windows.Forms.TextBox()
+        Me.txtPhoneExt = New System.Windows.Forms.TextBox()
+        Me.txtPhoneNumb = New System.Windows.Forms.TextBox()
+        Me.txtEmpNumb = New System.Windows.Forms.TextBox()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.txtMidName = New System.Windows.Forms.TextBox()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.LabelCombo = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnSaveRec = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.ErrorProvider1 = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.cboDeptName)
+        Me.GroupBox1.Controls.Add(Me.txtEmailAddy)
+        Me.GroupBox1.Controls.Add(Me.txtPhoneExt)
+        Me.GroupBox1.Controls.Add(Me.txtPhoneNumb)
+        Me.GroupBox1.Controls.Add(Me.txtEmpNumb)
+        Me.GroupBox1.Controls.Add(Me.txtLastName)
+        Me.GroupBox1.Controls.Add(Me.txtMidName)
+        Me.GroupBox1.Controls.Add(Me.txtFirstName)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.LabelCombo)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(54, 51)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(773, 623)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Enter Employee Data"
+        '
+        'cboDeptName
+        '
+        Me.cboDeptName.FormattingEnabled = True
+        Me.cboDeptName.Location = New System.Drawing.Point(338, 353)
+        Me.cboDeptName.Name = "cboDeptName"
+        Me.cboDeptName.Size = New System.Drawing.Size(396, 45)
+        Me.cboDeptName.TabIndex = 15
+        '
+        'txtEmailAddy
+        '
+        Me.txtEmailAddy.Location = New System.Drawing.Point(338, 548)
+        Me.txtEmailAddy.Name = "txtEmailAddy"
+        Me.txtEmailAddy.Size = New System.Drawing.Size(396, 44)
+        Me.txtEmailAddy.TabIndex = 14
+        '
+        'txtPhoneExt
+        '
+        Me.txtPhoneExt.Location = New System.Drawing.Point(338, 483)
+        Me.txtPhoneExt.Name = "txtPhoneExt"
+        Me.txtPhoneExt.Size = New System.Drawing.Size(396, 44)
+        Me.txtPhoneExt.TabIndex = 13
+        '
+        'txtPhoneNumb
+        '
+        Me.txtPhoneNumb.Location = New System.Drawing.Point(338, 418)
+        Me.txtPhoneNumb.Name = "txtPhoneNumb"
+        Me.txtPhoneNumb.Size = New System.Drawing.Size(396, 44)
+        Me.txtPhoneNumb.TabIndex = 12
+        '
+        'txtEmpNumb
+        '
+        Me.txtEmpNumb.Location = New System.Drawing.Point(338, 288)
+        Me.txtEmpNumb.Name = "txtEmpNumb"
+        Me.txtEmpNumb.Size = New System.Drawing.Size(396, 44)
+        Me.txtEmpNumb.TabIndex = 11
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Location = New System.Drawing.Point(338, 223)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(396, 44)
+        Me.txtLastName.TabIndex = 10
+        '
+        'txtMidName
+        '
+        Me.txtMidName.Location = New System.Drawing.Point(338, 158)
+        Me.txtMidName.Name = "txtMidName"
+        Me.txtMidName.Size = New System.Drawing.Size(396, 44)
+        Me.txtMidName.TabIndex = 9
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(338, 93)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(396, 44)
+        Me.txtFirstName.TabIndex = 8
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(98, 554)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(205, 31)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "E-mail Address:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(162, 489)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(141, 31)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "Extension:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(144, 424)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(150, 31)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Telephone:"
+        '
+        'LabelCombo
+        '
+        Me.LabelCombo.AutoSize = True
+        Me.LabelCombo.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelCombo.Location = New System.Drawing.Point(130, 359)
+        Me.LabelCombo.Name = "LabelCombo"
+        Me.LabelCombo.Size = New System.Drawing.Size(164, 31)
+        Me.LabelCombo.TabIndex = 4
+        Me.LabelCombo.Text = "Department:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(49, 294)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(245, 31)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Employee Number:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(141, 229)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(153, 31)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Last Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(114, 164)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(180, 31)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Middle Name:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(139, 99)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(155, 31)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "First Name:"
+        '
+        'btnSaveRec
+        '
+        Me.btnSaveRec.Location = New System.Drawing.Point(54, 700)
+        Me.btnSaveRec.Name = "btnSaveRec"
+        Me.btnSaveRec.Size = New System.Drawing.Size(203, 106)
+        Me.btnSaveRec.TabIndex = 1
+        Me.btnSaveRec.Text = "&Save Record"
+        Me.btnSaveRec.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(339, 700)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(203, 106)
+        Me.btnClear.TabIndex = 2
+        Me.btnClear.Text = "&Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(624, 700)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(203, 106)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'ErrorProvider1
+        '
+        Me.ErrorProvider1.ContainerControl = Me
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(881, 828)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnSaveRec)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Add Employee Data"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents LabelCombo As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtMidName As TextBox
+    Friend WithEvents txtFirstName As TextBox
+    Friend WithEvents txtEmailAddy As TextBox
+    Friend WithEvents txtPhoneExt As TextBox
+    Friend WithEvents txtPhoneNumb As TextBox
+    Friend WithEvents txtEmpNumb As TextBox
+    Friend WithEvents txtLastName As TextBox
+    Friend WithEvents cboDeptName As ComboBox
+    Friend WithEvents btnSaveRec As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents ErrorProvider1 As ErrorProvider
+End Class
---
+++ EmployeeData/Form1.vb
@@ -0,0 +1,140 @@
+﻿'Employee Data
+'William Butler
+'1/22/20
+'A program to add info about an employee into a text document for storage and record
+
+Option Strict On
+Imports System
+Imports System.IO
+
+Public Class Form1
+
+    '   Set variables as needed to hold filepath, filename, combobox options and an array to loop through for info needed writing the text file
+    Dim empFileName As String
+    ReadOnly filePath As String = My.Computer.FileSystem.SpecialDirectories.MyDocuments
+    ReadOnly dptNames() As String = {"Accounting", "Administration", "Marketing", "MIS", "Sales"}
+    ReadOnly emplabels() As String = {"First Name: ", "Middle Name: ", "Last Name: ", "Employee Number: ",
+                                      "Department: ", "Telephone: ", "Extension: ", "E-mail Address: "}
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        '   Input box for user to add a name for the text file for the employee record
+        '   If the file doesn't exist, create it, close it so its ready for use.
+        empFileName = InputBox("Please enter a filename for the employee record: ")
+        If Not File.Exists(empFileName) Then
+            File.CreateText(filePath & "\" & empFileName & ".txt").Dispose()
+        End If
+
+        '   Populate the combobox items
+        For Each dept In dptNames
+            cboDeptName.Items.Add(dept)
+        Next
+
+    End Sub
+
+    Private Sub TextBox_Validating(sender As Object, e As ComponentModel.CancelEventArgs) Handles cboDeptName.Validating, txtEmailAddy.Validating, txtEmpNumb.Validating, txtFirstName.Validating,
+                                                                                                  txtLastName.Validating, txtMidName.Validating, txtPhoneExt.Validating, txtPhoneNumb.Validating
+
+        '   Sets a check to make sure each textbox has info entered into it by looping through all
+        '   controls listed in handles if not uses error provider control
+        Dim ctl As Control = CType(sender, Control)
+        If ctl.Text = "" Then
+            e.Cancel = True
+            ErrorProvider1.SetError(ctl, "Please fill out all fields.")
+        End If
+
+    End Sub
+
+    Private Function IsItNumericPositive() As Boolean
+
+        '   Checks to make sure the info entered into phone number and extension are numbers
+        If Not IsNumeric(txtPhoneExt.Text) Then
+            MessageBox.Show("The extension field accepts numbers only.")
+            Return False
+        ElseIf Not IsNumeric(txtPhoneNumb.Text) Then
+            MessageBox.Show("The phone number field accepts numbers only.")
+            Return False
+        End If
+
+        If Val(txtPhoneNumb.Text) < 0 Then
+            MessageBox.Show("Phone number cannot be a negative number.")
+            Return False
+        ElseIf Val(txtPhoneExt.Text) < 0 Then
+            MessageBox.Show("Phone extension cannot be a negative number.")
+            Return False
+        End If
+
+        Return True
+    End Function
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        '   Closes the program
+        Me.Close()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        '   Clears all text boxes and resets combobox to blank
+        ClearAllTxt()
+    End Sub
+
+    Private Sub btnSaveRec_Click(sender As Object, e As EventArgs) Handles btnSaveRec.Click
+
+        '   Runs the validating sub, checks to make sure number and extension are numerical/not negative and gathers textbox input
+        ErrorProvider1.Clear()
+
+        If Not IsItNumericPositive() Then Exit Sub
+
+        If Me.ValidateChildren() Then
+            GatherTextInput()
+        End If
+
+    End Sub
+
+    Private Sub ClearAllTxt()
+
+        '   Sets a simple way to loop through each control if it is a textbox and clears it until all are cleared
+        '   Sets a variable for the loop to go through each control
+        Dim ctrl As Control = Me.GetNextControl(Me, True)
+
+        '   Starts a loop until the control specified are nothing
+        Do Until ctrl Is Nothing
+            '   If the type of control is textbox use clear() on that textbox
+            If TypeOf ctrl Is TextBox Then
+                DirectCast(ctrl, TextBox).Clear()
+            End If
+            '   Goes to next control
+            ctrl = Me.GetNextControl(ctrl, True)
+        Loop
+        cboDeptName.SelectedItem = Nothing
+
+    End Sub
+
+    Private Sub GatherTextInput()
+
+        '   Sets a list of strings with the textbox inputs, formatting the phone number string.
+        Dim tempstring As New List(Of String)
+        tempstring.Add(txtFirstName.Text)
+        tempstring.Add(txtMidName.Text)
+        tempstring.Add(txtLastName.Text)
+        tempstring.Add(txtEmpNumb.Text)
+        tempstring.Add(cboDeptName.SelectedItem.ToString)
+        tempstring.Add(String.Format("{0:(###) ###-####}", Long.Parse(txtPhoneNumb.Text)))
+        tempstring.Add(txtPhoneExt.Text)
+        tempstring.Add(txtEmailAddy.Text)
+
+        '   Calls to write to the file created in the form load event in the given path of my documents
+        '   and append to the file
+        Using infoWriter As New StreamWriter(filePath & "\" & empFileName & ".txt", True)
+
+            '   Loops through the emplabel array and tempstring array  to make a formatted entry into the textfile
+            For x = 0 To 7
+                infoWriter.WriteLine(emplabels(x).ToString & " " & tempstring(x))
+            Next
+
+        End Using
+        '   Lets the user know the text file was created and information entered
+        MessageBox.Show("Employee record for " & tempstring(0) & " " & tempstring(2) & " created sucessfully.")
+
+    End Sub
+
+End Class
